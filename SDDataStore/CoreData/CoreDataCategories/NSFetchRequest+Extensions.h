036056dda9012b37dc3435fc8c4f8786cc680d2d
Seth <seth@developer.com>
2014-10-27 18:00:25 -0700

    Removed method call from app delegate and added methods to NSFetchRequest category.

--- SDDataStore/CoreData/CoreDataCategories/NSFetchRequest+Extensions.h
+++ SDDataStore/CoreData/CoreDataCategories/NSFetchRequest+Extensions.h
@@ -10,4 +10,25 @@
 
 @interface NSFetchRequest (Extensions)
 
++(NSFetchRequest*) requestForEntity:(NSString*)className inContext:(NSManagedObjectContext*)ctx;
+
+/*!
+ *  Creates a fetch request and sets the entity, context and predicate if provided.
+ *
+ *  @param className The name of the entity the fetch is being executed for.
+ *  @param ctx       The context to perform the fetch in.
+ *  @param predicate A predicate string or nil. If a string is provided using variable substitution be sure to quote the variable. 
+                        EX. @"SELF beginsWith '%@'".
+ *
+ *  @return Returns an instance of NSFetchRequest.
+ */
++(NSFetchRequest*) requestForEntity:(NSString*)className inContext:(NSManagedObjectContext*)ctx withPredicateString:(NSString*)predicate;
+
+
+
+
++(NSFetchRequest*) requestForEntity:(NSString *)className inContext:(NSManagedObjectContext *)ctx withPredicate:(NSPredicate *)predicate;
+
+
+
 @end
--- SDDataStore/CoreData/CoreDataCategories/NSFetchRequest+Extensions.m
+++ SDDataStore/CoreData/CoreDataCategories/NSFetchRequest+Extensions.m
@@ -10,4 +10,69 @@
 
 @implementation NSFetchRequest (Extensions)
 
+
+
+
+
++(NSFetchRequest*) requestForEntityOfType:(NSString*)className inContext:(NSManagedObjectContext*)ctx
+{
+    // setup our fetch request
+    NSFetchRequest *request = [[NSFetchRequest alloc]init];
+    
+    // initialize our entity description
+    NSEntityDescription *entity = [NSEntityDescription entityForName:className inManagedObjectContext:ctx];
+    
+    // set the entity
+    [request setEntity:entity];
+    
+    return request;
+}
+
+
++(NSFetchRequest*) requestForEntity:(NSString *)className inContext:(NSManagedObjectContext *)ctx withPredicateString:(NSString *)predicate
+{
+    // setup our fetch request
+    NSFetchRequest *request = [[NSFetchRequest alloc]init];
+    
+    // initialize our entity description
+    NSEntityDescription *entity = [NSEntityDescription entityForName:className inManagedObjectContext:ctx];
+    
+    // set the entity
+    [request setEntity:entity];
+    
+    if (predicate)
+    {
+        // setup our predicate to find our specific user
+        NSPredicate *pred = [NSPredicate predicateWithFormat:predicate];
+        
+        // set the predicate
+        [request setPredicate:pred];
+    }
+    
+    
+    return request;
+}
+
+
++(NSFetchRequest*) requestForEntity:(NSString *)className inContext:(NSManagedObjectContext *)ctx withPredicate:(NSPredicate *)predicate
+{
+    // setup our fetch request
+    NSFetchRequest *request = [[NSFetchRequest alloc]init];
+    
+    // initialize our entity description
+    NSEntityDescription *entity = [NSEntityDescription entityForName:className inManagedObjectContext:ctx];
+    
+    // set the entity
+    [request setEntity:entity];
+    
+    if (predicate)
+    {
+        // set the predicate
+        [request setPredicate:predicate];
+    }
+    
+    return request;
+}
+
+
 @end
