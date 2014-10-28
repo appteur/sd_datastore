4d4c8d2d4c57aa06cae342a0374167683f85d34d
Seth <seth@developer.com>
2014-10-27 18:01:58 -0700

    Started stubbing out some example methods in SDCoreDataInterface.

--- SDDataStore/CoreData/SDCoreDataInterface.m
+++ SDDataStore/CoreData/SDCoreDataInterface.m
@@ -7,7 +7,75 @@
 //
 
 #import "SDCoreDataInterface.h"
+#import "SDCoreDataLayer.h"
+
+//categories
+#import "NSFetchRequest+Extensions.h"
+#import "NSManagedObjectContext+Extensions.h"
+#import "NSManagedObject+Extensions.h"
+#import "NSObject+Extensions.h"
+
+@interface SDCoreDataInterface()
+
+@property (nonatomic, strong) SDCoreDataLayer *cdlayer;
+@property (nonatomic, strong) NSManagedObjectContext *ctx;
+
+@end
 
 @implementation SDCoreDataInterface
+@synthesize ctx;
+
+
+-(instancetype) init
+{
+    if (self = [super init])
+    {
+        self.cdlayer = [SDCoreDataLayer singleton];
+        self.ctx = self.cdlayer.managedObjectContext;
+    }
+    return self;
+}
+
+// TODO: this should return a "User" entity object
+-(id) userWithIdentifier:(NSString*)userId
+{
+    id user = [ctx uniqueEntityOfType:@"User" matchingPredicate:[NSPredicate predicateWithFormat:@"userId == %@", userId]];
+    
+    return user;
+}
+
+
+-(NSDictionary*) userInfoFromUserId:(NSString*)userId
+{
+    NSManagedObject *obj = [ctx uniqueEntityOrNilOfType:@"User" matchingPredicate:[NSPredicate predicateWithFormat:@"userId == %@", userId]];
+    
+    return [obj toDictionary];
+}
+
+
+-(void) updateAvatar:(NSString*)newURL forUserId:(NSString*)userId
+{
+    [ctx updateAttribute:@"avatar" withValue:newURL onEntity:@"User" matchingPredicate:[NSPredicate predicateWithFormat:@"userId == %@", userId]];
+    
+    [self.cdlayer saveContext];
+}
+
+
+-(void) deleteChildWithId:(NSString*)childId
+{
+    [ctx deleteEntityOfType:@"User" matchingPredicate:[NSPredicate predicateWithFormat:@"UserId == '%@'",childId]];
+    
+    [self.cdlayer saveContext];
+}
+
+
+- (void) deleteAllObjectsOfType:(NSString *)entityName
+{
+    [ctx deleteAllEntitiesOfType:entityName];
+    
+    [self.cdlayer saveContext];
+}
+
+
 
 @end
