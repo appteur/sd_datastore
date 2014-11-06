8e458f8782d6a77c8050cfacf406a1987bff4ffe
Seth <seth@developer.com>
2014-11-06 15:25:39 -0800

    Added image color sampler code as an exercise. To be removed.

---
+++ SDDataStore/ImageSampleTest.h
@@ -0,0 +1,13 @@
+//
+//  ImageSampleTest.h
+//  SDDataStore
+//
+//  Created by Seth on 11/6/14.
+//  Copyright (c) 2014 Seth. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ImageSampleTest : NSObject
+
+@end
